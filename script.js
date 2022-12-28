d1e6828398b21dd712254596cadd99940fda201f
DK0280705 <deka.ganteng028@gmail.com>
2022-12-29 03:22:39 +0700

    update

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 (() => {
-    const title = "The name is DK"
+    const title = [ "Yo", ", the name is DK" ];
 
     window.onload = (event) => {
         document.getElementById("black").style.display = "none";
 
-        let i = 0;
+        let i = 0, j = 0;
         const titleTyping = () => {
-            if (i < title.length) {
+            if (i < title[j].length) {
                 document.getElementById("main-title").style.animation = "";
-                document.getElementById("main-title").innerHTML += title.charAt(i);
+                document.getElementById("main-title").innerHTML += title[j].charAt(i);
                 i++;
                 setTimeout(titleTyping, 100);
+            } else if (j < (title.length - 1)) {
+                i = 0;
+                j++;
+                document.getElementById("main-title").style.animation = "blink-caret 1s steps(1) infinite";
+                setTimeout(titleTyping, 1000);
             } else {
                 document.getElementById("main-title").style.animation = "blink-caret 1s steps(1) infinite";
             }
